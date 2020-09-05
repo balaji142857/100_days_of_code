a7cc8bd3b2786a8f963d8949b0b98371619b6f0a
Balaji Krishnan <balaji2k9_srec14@ymail.com>
2020-09-05 16:40:52 +0530

    Selection Sort

---
+++ sorting/src/main/java/com/krishnan/balaji/SelectionSort.java
@@ -0,0 +1,37 @@
+package com.krishnan.balaji;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectionSort<T extends Comparable<T>> implements Sort<T> {
+
+	@Override
+	public List<T> sort(List<T> input) {
+		List<T> output = new ArrayList<>(input.size());
+		while(input.size() > 0) {
+			int index = findMinIndex(input);
+			T item = input.remove(index);
+			output.add(item);
+			
+		}
+		return output;
+	}
+
+	private int findMinIndex(List<T> input) {
+		T minSoFar = null;
+		int minIndex = 0;
+		for (int i = 0; i < input.size(); i++) {
+			if ( null == minSoFar) {
+				minSoFar = input.get(i);
+				minIndex = i;
+				continue;
+			}
+			if(minSoFar.compareTo(input.get(i))  > 0) {
+				minSoFar = input.get(i);
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+
+}
