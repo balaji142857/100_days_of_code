2d893651e0230d3be8ab66d74f984a89a7804c33
Balaji Krishnan <5593849+balaji142857@users.noreply.github.com>
2020-10-21 00:22:10 +0530

    concurrency  & executors

--- sorting/src/main/java/com/kb/j7/concurrency/Test.java
+++ sorting/src/main/java/com/kb/j7/concurrency/Test.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class Test {
 	public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
 
-//		CyclicBarrier cb;
-//		Semaphore s;
-//		Phaser p;
-//		Exchanger ex;
 		System.out.println("countDownLatch: ");
 		testCountDownLatch();
 		System.out.println("cyclicBarrier: ");
@@ -28,24 +28,94 @@ public class Test {
 		testPhaser();
 		System.out.println("semaphore");
 		testSemaphore();
-		System.out.println("executors + fkJoin");
+		System.out.println("exchanger");
+		testExchanger();
+		System.out.println("executors + fkJoin + shutdown/shutdownNow");
 		testExecutors();
-
 	}
 
-	private static void testExecutors() {
-		// TODO Auto-generated method stub
-		
+	private static void testExchanger() throws InterruptedException {
+		Exchanger<String> e = new Exchanger<>();
+		ExecutorService es = Executors.newFixedThreadPool(1);
+		es.submit(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(3 + Math.round(Math.random() * 10));
+				System.out.println(e.exchange("from runnable"));
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		});
+		es.shutdown();
+		System.out.println("initiating exchange at " + System.currentTimeMillis());
+		System.out.println(e.exchange("from main"));
+		System.out.println("exchange completed at " + System.currentTimeMillis());
 	}
 
-	private static void testSemaphore() {
-		// TODO Auto-generated method stub
-		
+	private static void testExecutors()  {
+		ExecutorService es = Executors.newFixedThreadPool(4);
+		/**
+		 * One good way to shut down the ExecutorService 
+		 * is to use both of these methods combined with the awaitTermination() method.
+		 * 
+		 * shutdown
+		 * try 
+		 * 		awaitTermination
+		 * catch
+		 * 		shutDownNow
+		 *  
+		 */
+		es.submit(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(5  + Math.round(Math.random() * 5));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		 // shutdown - wont block calling thread, wont accept new requests - but ongoing tasks will NOT BE interrupted
+		es.shutdown();
+		/**
+		 * await termination - block current thread until either the ES shuts down or time expires
+ 		 * whichever happens first. if time expired exception will be thrown
+		 */
+		try {
+			es.awaitTermination(3, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			 // shutdownNow same as shutdown - diff is this WILL interrupt the ongoing tasks
+			es.shutdownNow();
+		}
+		CompletableFuture cf;
+	}
+
+	private static void testSemaphore() throws InterruptedException {
+		Semaphore sem = new Semaphore(4);
+		sem.acquire();//acquire 1 permit. if no permits are available calling thread is blocked
+		sem.acquire(2);//acquire n permits
+		sem.tryAcquire();//acquire - but if no available permits return boolean - will not block
+		sem.release();// release permit
+		sem.acquireUninterruptibly();//same as acquire - but interrupting the thread will not unblock
+		sem.release(2);
+		sem.release();
+		System.out.println("semaphore test finished");
 	}
 
 	private static void testPhaser() {
-		// TODO Auto-generated method stub
-		
+		int threadCount = Runtime.getRuntime().availableProcessors();
+		ExecutorService es = Executors.newFixedThreadPool(threadCount);
+		Thread.currentThread().setName("main-1-thread-1");
+		Phaser p = new Phaser();
+		System.out.println("after phaser construction");
+		printPhaserDetails(Thread.currentThread().getName(), p);
+		p.register();
+		System.out.println("after phaser register");
+		printPhaserDetails(Thread.currentThread().getName(), p);
+		IntStream.range(0, threadCount).forEach(item -> es.submit(new MyPhaserCallable(p)));
+		printPhaserDetails(Thread.currentThread().getName() + " arrived1", p);
+		p.arriveAndAwaitAdvance();
+		printPhaserDetails(Thread.currentThread().getName() + " Arrive&Advance", p);
+		p.arriveAndDeregister();
+		printPhaserDetails(Thread.currentThread().getName() + " deregistered", p);
+		es.shutdown();
+		Thread.currentThread().setName("main");
 	}
 
 	private static void testCyclicBarrier() throws InterruptedException, BrokenBarrierException {
@@ -55,7 +125,7 @@ public class Test {
 			System.out.println(Thread.currentThread().getName() + " barrier completed action");
 		});
 		List<Future<String>> result = new ArrayList<>();
-		IntStream.range(0, 4).forEach(item -> result.add(es.submit(new MyDiffCallable(cb))) );
+		IntStream.range(0, 4).forEach(item -> result.add(es.submit(new MyBarrierCallable(cb))));
 		es.shutdown();
 		result.stream().forEach(future -> {
 			try {
@@ -65,7 +135,7 @@ public class Test {
 			}
 		});
 		System.out.println("cyclic barrier resets once all threads have called await,");
-		
+
 	}
 
 	private static void testCountDownLatch() throws InterruptedException {
@@ -74,10 +144,11 @@ public class Test {
 		CountDownLatch cl = new CountDownLatch(threadCount);
 
 		List<Future<String>> result = new ArrayList<>();
-		IntStream.range(0, 4).forEach(item -> result.add(es.submit(new MyCallable(cl))) );
-		// if timeout exceeds exception is not thrown, return value is false... await() return type is void btw
+		IntStream.range(0, 4).forEach(item -> result.add(es.submit(new MyLatchCallable(cl))));
+		// if timeout exceeds exception is not thrown, return value is false... await()
+		// return type is void btw
 		boolean waitResult = cl.await(10, TimeUnit.SECONDS);
-		System.out.println("wait result is"+ waitResult);
+		System.out.println("wait result is" + waitResult);
 		System.out.println("additional await calls on the same latch has no effect"
 				+ " - counter will reamain at zero and will not reset once all threads have called the countDown()");
 		cl.await();
@@ -93,16 +164,21 @@ public class Test {
 				e.printStackTrace();
 			}
 		});
-		System.out.println("countDownLatch will not reset even after all threads call caountDown, count:" +cl.getCount());
+		System.out.println(
+				"countDownLatch will not reset even after all threads call caountDown, count:" + cl.getCount());
 	}
 
+	public static void printPhaserDetails(String threadName, Phaser p) {
+		System.out.println(threadName + "\t" + System.currentTimeMillis() + "\t:::\t arrivedParties: "
+				+ p.getArrivedParties() + ",unarrivedParties " + p.getUnarrivedParties() + ", phase: " + p.getPhase());
+	}
 }
 
-class MyCallable implements Callable<String> {
+class MyLatchCallable implements Callable<String> {
 
 	private CountDownLatch latch = null;
 
-	MyCallable(CountDownLatch latch) {
+	MyLatchCallable(CountDownLatch latch) {
 		this.latch = latch;
 	}
 
@@ -115,12 +191,11 @@ class MyCallable implements Callable<String> {
 	}
 }
 
+class MyBarrierCallable implements Callable<String> {
 
-class MyDiffCallable implements Callable<String> {
+	private CyclicBarrier cb = null;
 
-	private CyclicBarrier cb= null;
-
-	MyDiffCallable(CyclicBarrier cb) {
+	MyBarrierCallable(CyclicBarrier cb) {
 		this.cb = cb;
 	}
 
@@ -128,9 +203,35 @@ class MyDiffCallable implements Callable<String> {
 	public String call() throws Exception {
 		long sleepTime = Math.round(10 * Math.random());
 		TimeUnit.SECONDS.sleep(sleepTime);
-		System.out.println(Thread.currentThread().getName()+" calling cb.await");
+		System.out.println(Thread.currentThread().getName() + " calling cb.await");
 		cb.await();
-		System.out.println(Thread.currentThread().getName()+" cb.await completed");
+		System.out.println(Thread.currentThread().getName() + " cb.await completed");
 		return Thread.currentThread().getName() + " slept for " + sleepTime;
 	}
 }
+
+class MyPhaserCallable implements Callable<String> {
+
+	private Phaser p;
+
+	MyPhaserCallable(Phaser p) {
+		this.p = p;
+	}
+
+	@Override
+	public String call() throws Exception {
+		Test.printPhaserDetails(Thread.currentThread().getName() + " before register", p);
+		p.register();
+		Test.printPhaserDetails(Thread.currentThread().getName() + " after register", p);
+		TimeUnit.SECONDS.sleep(Math.round(Math.random()*10));
+		Test.printPhaserDetails(Thread.currentThread().getName() + " arrived1", p);
+		p.arriveAndAwaitAdvance();
+		TimeUnit.SECONDS.sleep(Math.round(Math.random()*10));
+		p.arriveAndDeregister();
+		Test.printPhaserDetails(Thread.currentThread().getName() + " after arr&dereg", p);
+		return null;
+	}
+
+}
+
+
